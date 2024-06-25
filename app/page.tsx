20e2f4f8add7920553209f56e3cd2f4b6687c802
Roberto Castillo Medina <betomax_16@live.com.mx>
2024-06-25 12:28:44 -0600

    feat: :recycle: Creation of structure for page sections
    Creation of structure for page sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,12 @@
 "use client";
 
-import { useTheme } from "next-themes";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import Image from "next/image";
 
-import imageWeb from "@/public/web.svg";
-import ButtonAnimateBackgound from "@/components/ButtonAnimate/Background/ButtonAnimateBackgound";
-import { Component3D } from "@/components/Component3D/Component3D";
-import { ScrollComponent } from "@/components/ScrollComponent/ScrollComponent";
-import styles from "@/styles/Home/Home.module.css";
-
+import { FirstSection } from "@/pages/home/section1/Section1";
 library.add(fab);
 dom.watch();
 
 export default function Home() {
-  const { theme } = useTheme();
-
-  return (
-    <section
-      className={`${styles.section} ${theme == "dark" ? styles.firstSecctionDark : styles.firstSecctionLigth}`}
-    >
-      <div className={`${styles.container} ${styles.firstContainer}`}>
-        <div className={styles.boxContainerLeft}>
-          <Component3D>
-            <Image
-              priority
-              alt="Web devices"
-              className={`${styles.image}`}
-              src={imageWeb}
-            />
-          </Component3D>
-        </div>
-        <div className={styles.boxContainerRight}>
-          <h1
-            className={`${styles.title} ${theme == "dark" ? styles.dark : ""}`}
-          >
-            Build stunning websites & apps.
-          </h1>
-          <span
-            className={`${styles.subtitle} ${theme == "dark" ? styles.dark : ""}`}
-          >
-            Create live segments and target the right people for messages based
-            on their behaviors.
-          </span>
-          <ScrollComponent>
-            <ButtonAnimateBackgound style={theme} text="View Showcase" />
-          </ScrollComponent>
-        </div>
-      </div>
-    </section>
-  );
+  return <FirstSection />;
 }
---
+++ pages/home/section1/Section1.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+import imageWeb from "@/public/web.svg";
+import ButtonAnimateBackgound from "@/components/ButtonAnimate/Background/ButtonAnimateBackgound";
+import { Component3D } from "@/components/Component3D/Component3D";
+import { ScrollComponent } from "@/components/ScrollComponent/ScrollComponent";
+import styles from "@/pages/home/section1/Section1.module.css";
+
+export function FirstSection() {
+  const { theme } = useTheme();
+
+  return (
+    <section
+      className={`${styles.section} ${theme == "dark" ? styles.firstSecctionDark : styles.firstSecctionLigth}`}
+    >
+      <div className={`${styles.container} ${styles.firstContainer}`}>
+        <div className={styles.boxContainerLeft}>
+          <Component3D>
+            <Image
+              priority
+              alt="Web devices"
+              className={`${styles.image}`}
+              src={imageWeb}
+            />
+          </Component3D>
+        </div>
+        <div className={styles.boxContainerRight}>
+          <h1
+            className={`${styles.title} ${theme == "dark" ? styles.dark : ""}`}
+          >
+            Build stunning websites & apps.
+          </h1>
+          <span
+            className={`${styles.subtitle} ${theme == "dark" ? styles.dark : ""}`}
+          >
+            Create live segments and target the right people for messages based
+            on their behaviors.
+          </span>
+          <ScrollComponent>
+            <ButtonAnimateBackgound style={theme} text="View Showcase" />
+          </ScrollComponent>
+        </div>
+      </div>
+    </section>
+  );
+}
