8f685d01ba12db6e140aed4e80a67d7c04e390dc
Roberto Castillo Medina <betomax_16@live.com.mx>
2024-07-07 21:30:36 -0600

    feat: :sparkles: Add fourth section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { FirstSection } from "@/pages/home/section1/Section1";
 import { SecondSection } from "@/pages/home/section2/Section2";
 import { ThirdSection } from "@/pages/home/section3/Section3";
+import { FourthSection } from "@/pages/home/section4/Section4";
 library.add(fab);
 dom.watch();
 
@@ -15,6 +16,7 @@ export default function Home() {
       <FirstSection />
       <SecondSection />
       <ThirdSection />
+      <FourthSection />
     </>
   );
 }
