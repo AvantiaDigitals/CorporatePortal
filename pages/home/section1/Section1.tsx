909c22210183a453dd5323428678d747a9022c2f
Roberto Castillo <avantia.digitalis@hotmail.com>
2024-07-21 23:39:52 -0600

    style: :lipstick: Update styles for first section main page

--- pages/home/section1/Section1.tsx
+++ pages/home/section1/Section1.tsx
@@ -16,32 +16,33 @@ export default function FirstSection() {
     <section
       className={`${styles.section} ${theme == "dark" ? styles.firstSecctionDark : styles.firstSecctionLigth}`}
     >
-      <div className={`${styles.container} ${styles.firstContainer}`}>
-        <div className={styles.boxContainerLeft}>
-          <Component3D>
-            <Image
-              priority
-              alt="Web devices"
-              className={`${styles.image}`}
-              src={imageWeb}
-            />
-          </Component3D>
-        </div>
-        <div className={styles.boxContainerRight}>
-          <h1
-            className={`${styles.title} ${theme == "dark" ? styles.dark : ""}`}
-          >
-            Build stunning websites & apps.
-          </h1>
-          <span
-            className={`${styles.subtitle} ${theme == "dark" ? styles.dark : ""}`}
-          >
-            Create live segments and target the right people for messages based
-            on their behaviors.
-          </span>
-          <ScrollComponent>
-            <ButtonAnimateBackgound style={theme} text="View Showcase" />
-          </ScrollComponent>
+      <div className={`${styles.container}`}>
+        <div className={styles.firstContainer}>
+          <div className={styles.boxContainerLeft}>
+            <Component3D>
+              <Image
+                priority
+                alt="Web devices"
+                className={`${styles.image}`}
+                src={imageWeb}
+              />
+            </Component3D>
+          </div>
+          <div className={styles.boxContainerRight}>
+            <h1
+              className={`${styles.title} ${theme == "dark" ? styles.dark : ""}`}
+            >
+              Build stunning websites & apps.
+            </h1>
+            <span
+              className={`${styles.subtitle} ${theme == "dark" ? styles.dark : ""}`}
+            >
+              Create live segments and target the right people for messages based on their behaviors.
+            </span>
+            <ScrollComponent>
+              <ButtonAnimateBackgound style={theme} text="View Showcase" />
+            </ScrollComponent>
+          </div>
         </div>
       </div>
     </section>
